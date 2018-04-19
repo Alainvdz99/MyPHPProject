74bca7ca360f4a3d567308ffb024a137de41d647
Alain <zandalain@live.nl>
2018-04-19 22:24:11 +0200

    New php update

---
+++ mysql/db.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$connection = Mysqli_connect('localhost' , 'root', '', 'loginapp');
+
+    if(!$connection) {
+
+
+    die("database connection failed");
+    }
+
+
+
+?>
---
+++ mysql/login_update.php
@@ -0,0 +1,85 @@
+<?php include "db.php";?>
+<?php include "functions.php"?>
+
+<?php
+if(isset($_POST['submit'])) {
+
+    UpdateTable();
+}
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container">
+
+     <div class="col-xs-6">
+
+
+
+      <div class="container">
+
+     <div class="col-xs-6">
+         <form action="login.php" method="post">
+
+             <div class="form-group">
+                <label for="username">Username</label>
+                 <input type="text" name="username" class="form-control">
+
+
+             </div>
+
+             <div class="form-group">
+                <label for="password">Password</label>
+                 <input type="password" name="password" class="form-control">
+
+
+             </div>
+
+              <div class="form-group">
+
+                 <select name="id" id="">
+
+
+                <?php
+
+          showAllData();
+
+                ?>
+
+
+                 </select>
+
+
+
+             </div>
+
+              <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
+
+             </div>
+
+
+         </form>
+
+
+     </div>
+
+ </div>
+
+
+
+
+</body>
+</html>
